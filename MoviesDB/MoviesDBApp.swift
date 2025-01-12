d36d1ed9f0201721fabc65b1dc7b919c5a4d066c
Elif Bilge Parlak <elf.prlk64@gmail.com>
2025-01-12 23:19:21 +0100

    Initial Commit

---
+++ MoviesDB/MoviesDBApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoviesDBApp.swift
+//  MoviesDB
+//
+//  Created by Elif Parlak on 12.01.2025.
+//
+
+import SwiftUI
+
+@main
+struct MoviesDBApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
